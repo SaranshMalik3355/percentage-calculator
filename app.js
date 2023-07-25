ec82f7272cfdf0b376ddef6c62a36953f119896c
Saransh Malik <saransh.malik.cs.2019@miet.ac.in>
2023-07-25 15:19:09 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+let percentage = document.getElementById("pro");
+
+percentage.addEventListener("keyup", function () {
+  let value = percentage.value;
+  console.log(value);
+});
+// console.log(value);
+
+// function getVal() {
+//   const value = percentage.value;
+//   console.log(value);
+// }
